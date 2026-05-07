7e0faf7420684e8d5bd78dc12ad7f52fdb69ea71
Shyamali-kumari-rai <shyamalikumarirai@gmail.com>
2026-05-07 14:31:35 +0000

    datatypes

---
+++ 1_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3+3);
+console.log("shyamali");
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint =>  for very large number
+// string =>""
+// boolean =>true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
